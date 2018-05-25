0302d0a66591e3765f129640e31202b5bb5633f3
Kamil Rykowski <kamil.rykowski@stxnext.pl>
2018-05-25 22:23:06 +0200

    Multiple locations

--- mobile/iAir/src/components/pages/HomePage.js
+++ mobile/iAir/src/components/pages/HomePage.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
+import Swiper from 'react-native-swiper';
+
 import WeatherCard from '../molecules/WeatherCard';
 
-const HomePage = ({ temperature, humidity, pm10, pm25, pm100 }) => {
+const HomePage = ({ items }) => {
   return (
-    <ImageBackground
-      source={require('../../assets/images/background.jpg')}
-      style={styles.container}
+    <Swiper
+      showsButtons={false}
+      loop={true}
+      autoplay={true}
+      style={styles.wrapper}
     >
-      <WeatherCard
-        city="Gdańsk"
-        temperature={temperature}
-        humidity={humidity}
-        pm10={pm10}
-        pm25={pm25}
-        pm100={pm100}
-      />
-    </ImageBackground>
+      {items.map((item, i) => {
+        return (
+          <View
+            key={i}
+            style={styles.slide}
+          >
+            <ImageBackground
+              source={require('../../assets/images/background.jpg')}
+              style={styles.container}
+            >
+              <WeatherCard
+                city={item.city}
+                temperature={item.temperature}
+                humidity={item.humidity}
+                pm10={item.pm10}
+                pm25={item.pm25}
+                pm100={item.pm100}
+              />
+            </ImageBackground>
+          </View>
+        );
+      })}
+    </Swiper>
   );
 };
 
 export default HomePage;
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  slide: {
+    flex: 1,
+    backgroundColor: '#9DD6EB',
   },
 });
--- mobile/iAir/src/screens/HomeScreen.js
+++ mobile/iAir/src/screens/HomeScreen.js
@@ -3,8 +3,39 @@ import React from 'react';
 import HomePage from '../components/pages/HomePage';
 
 class HomeScreen extends React.Component {
-  componentDidMount() {
+  state = {
+    items: [],
+  };
 
+  componentDidMount() {
+    const items = [
+      {
+        city: 'Gdańsk',
+        temperature: 30,
+        humidity: 50,
+        pm10: 2,
+        pm25: 5,
+        pm100: 41,
+      },
+      {
+        city: 'Łódź',
+        temperature: 10,
+        humidity: 0,
+        pm10: 1,
+        pm25: 2,
+        pm100: 3,
+      },
+      {
+        city: 'Piła',
+        temperature: -20,
+        humidity: 15,
+        pm10: 100,
+        pm25: 100,
+        pm100: 100,
+      },
+    ];
+
+    this.setState({ items });
   }
 
   componentWillUnmount() {
@@ -12,13 +43,11 @@ class HomeScreen extends React.Component {
   }
 
   render() {
+    const { items } = this.state;
+
     return (
       <HomePage
-        temperature={30}
-        humidity={68}
-        pm10={2}
-        pm25={5}
-        pm100={41}
+        items={items}
       />
     );
   }
